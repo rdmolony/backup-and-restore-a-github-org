366e4e0429452fe7f40222d90f93b9dc24bbad5f
Rowan Molony <rowanmolony@gmail.com>
2025-06-27 16:09:03 +0100

    build(nix): add claude-sync

--- flake.nix
+++ flake.nix
@@ -1,15 +1,22 @@
 {
   inputs = {
     utils.url = "github:numtide/flake-utils";
+    claude-sync = {
+      url = "github:rdmolony/sync-claude-code-with-github-issues/sync-deterministically";
+      inputs.utils.follows = "utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, utils, ... }@inputs: utils.lib.eachDefaultSystem (system:
     let
       pkgs = nixpkgs.legacyPackages.${system};
+      claude-sync = inputs.claude-sync.packages.${system}.default;
     in
     {
       devShell = pkgs.mkShell {
         buildInputs = with pkgs; [
           gh
+          claude-sync
         ];
       };
     }
